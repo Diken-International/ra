2d4175ef189bc1a0a6f1af8b09a4b7821ff5ab5d
J.A C.C <space_jam170@hotmail.com>
2020-06-01 11:57:00 -0500

    Messages Start

---
+++ database/migrations/2020_06_01_163840_create_messages_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMessagesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('messages', function (Blueprint $table) {
+            $table->increments('id');
+            $table->date('created_date');
+            $table->longText('message');
+            $table->integer('autor_id');
+            $table->integer('priority');
+            $table->integer('services_id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('messages');
+    }
+}
