83bfc93dc0eefea962d6de0348804506a543685c
Lockdown <al221411868@gmail.com>
2020-11-10 10:33:50 -0600

    check list

--- resources/views/formats/serial_number.blade.php
+++ resources/views/formats/serial_number.blade.php
@@ -6,26 +6,27 @@
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css">
   </head>
   <body>
-    <h2 class="mb-3">Customer List</h2>
-    <table class="table table-bordered">
-    <thead>
-      <tr>
-        <th>Name</th>
-        <th>E-mail</th>
-        <th>Phone</th>
-        <th>DOB</th>
-      </tr>
-      </thead>
+    <h2 class="mb-3">
+      <center>
+        {{ $number->name }}
+      </center>  
+    </h2>
+    <br>
+    <table class="table">
       <tbody>
        <tr>
-        <td>{{ $number->serial_number }}</td>
         <td>
           <div class="visible-print text-center">
-              <img src="data:image/png;base64, {{ base64_encode(QrCode::format('png')->size(100)->generate("$number->serial_number")) }} ">
+              <img src="data:image/png;base64, {{ base64_encode(QrCode::format('png')->size(500)->generate("$number->serial_number")) }} ">
           </div>
         </td>
        </tr> 
       </tbody>
     </table>
+    <h2>Informacion del Equipo</h2>
+    <br>
+    <br>
+    <h3>Numero de serie: {{ $number->serial_number }}</h3>
+    <h3>Empresa o Compañia: {{ $number->Bussines }}</h3>
   </body>
 </html>
